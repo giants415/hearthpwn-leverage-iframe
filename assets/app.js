c6eb9e28642a081bbd54a6282792193dd3ba1ab3
Andrew <andrew.vinocur@gmail.com>
2017-07-18 10:47:40 -0700

    creates source array that will be used to change iframe content

--- assets/app.js
+++ assets/app.js
@@ -2,14 +2,21 @@ console.log('app.js connected');
 
 $(window).on('load', function(){
 
+  var source = [
+    './test/page1.html',
+    './test/page2.html'
+  ];
+
+
   $('.primary').click(function(){
     console.log('primary fired');
-    $('#hearthpwn').attr('src', './test/page1.html');
+    console.log(source[0]);
+    $('#hearthpwn').attr('src', source[0]);
   });
 
   $('.secondary').click(function(){
     console.log('secondary fired');
-    $('#hearthpwn').attr('src', './test/page2.html');
+    $('#hearthpwn').attr('src', source[1]);
   });
 
 });
