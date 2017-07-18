0f207baf5fd77ec5206223e1f7744d2944dd061b
Andrew <andrew.vinocur@gmail.com>
2017-07-18 15:16:46 -0700

    adds logic to work with the counter and creates start over logic

--- assets/app.js
+++ assets/app.js
@@ -4,21 +4,30 @@ $(window).on('load', function(){
   var counter = 0;
 
   var source = [
+    './test/test.html',
     './test/page1.html',
     './test/page2.html'
   ];
 
-
-  $('.primary').click(function(){
-    console.log('primary fired');
-    counter++;
+  $('.disabled').click(function(){
+    console.log('start over fired');
+    counter = 0;
     console.log(counter);
     $('#hearthpwn').attr('src', source[0]);
   });
 
+  $('.primary').click(function(){
+    console.log('backwards fired');
+    counter--;
+    console.log(counter);
+    $('#hearthpwn').attr('src', source[counter]);
+  });
+
   $('.secondary').click(function(){
-    console.log('secondary fired');
-    $('#hearthpwn').attr('src', source[1]);
+    console.log('forwards fired');
+    counter++;
+    console.log(counter);
+    $('#hearthpwn').attr('src', source[counter]);
   });
 
 });
