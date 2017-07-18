80ed22e9c46ee8b3dbe6102e218a26eb148a53f6
Andrew <andrew.vinocur@gmail.com>
2017-07-17 20:55:11 -0700

    creates basic button jquery code

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,14 @@ console.log('app.js connected');
 
 $(window).on('load', function(){
 
+  $('.primary').click(function(){
+    console.log('primary fired');
+    $('#hearthpwn').attr('src', './test/page1.html');
+  });
+
+  $('.secondary').click(function(){
+    console.log('secondary fired');
+    $('#hearthpwn').attr('src', './test/page2.html');
+  });
 
 });
