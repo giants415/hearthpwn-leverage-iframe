6af6c54275c48431a6428f63602f728262040ebe
Andrew <andrew.vinocur@gmail.com>
2017-07-19 18:26:03 -0700

    all of this is for testing out the numerous custom functions which work

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,10 @@
 console.log('app.js connected');
 
 var source = {
-  'test':'./test.html',
   'page1':'./page1.html',
-  'page2':'./page2.html'
+  'page2':'./page2.html',
+  'page3':'./page3.html',
+  'page4':'./page4.html'
 };
 
 var counter = 0;
@@ -19,6 +20,23 @@ var android = function(){
 var pc_ios = function() {
   location.href = source.page2;
 }
+
+var nextpage = function() {
+  location.href = source.page3;
+}
+
+var prevpage = function() {
+  location.href = source.page2;
+}
+
+var nextpage2 = function(){
+  location.href = source.page4;
+}
+
+var prevpage2 = function(){
+  location.href = source.page1;
+}
+
 
 $(window).on('load', function(){
 
@@ -39,30 +57,31 @@ $(window).on('load', function(){
   //!!!!!!!! ENABLE FOR FINAL, DISABLED BC FIXED HEIGHT IS EASIER TO SEE !!!!!!!!//
   // $('#hearthpwn').css('height', $(window).height()+'px');
 
+  //MOVE DISBALED BUTTON FUNCTIONALITY TO A NEW "START OVER" A TAG//
 
 
-
-//MOVE DISBALED BUTTON FUNCTIONALITY TO A NEW "START OVER" A TAG//
-
-
-//NEED TO CHANGE THIS SO THIS BUTTON APPEARS BEFORE PLATFORM IS CHOSEN//
+  //NEED TO CHANGE THIS SO THIS BUTTON APPEARS BEFORE PLATFORM IS CHOSEN//
   $('.disabled').click(function(){
-    console.log('start over fired');
-    counter = 0;
-    console.log(counter);
-    $('#hearthpwn').attr('src', source[0]);
+    // console.log('start over fired');
+    // counter = 0;
+    // console.log(counter);
+    // $('#hearthpwn').attr('src', source[0]);
   });
 
   $('.primary').click(function(){
-
+    // console.log('next fired');
+    // counter++;
+    // console.log(counter);
+    // location.href = source[counter];
   });
 
   $('.secondary').click(function(){
-    console.log('previous fired');
-    counter--;
-    console.log(counter);
+    // console.log('previous fired');
+    // counter--;
+    // console.log(counter);
     // $('#hearthpwn').attr('src', source[counter]);
   });
+
 
   // foo = function() {
   //   console.log('yurp');
