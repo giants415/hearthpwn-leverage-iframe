c062576cbf3509b7b6dc3d953e734b2244049f21
Andrew <andrew.vinocur@gmail.com>
2017-07-20 14:16:43 -0700

    comments out window load and jquery cdn

--- assets/app.js
+++ assets/app.js
@@ -57,35 +57,36 @@ var pc_ios5 = function(){
   location.href = ios_pc.step5;
 }
 
-$(window).on('load', function(){
-  //!!!!!!!! ENABLE FOR FINAL, DISABLED BC FIXED HEIGHT IS EASIER TO SEE !!!!!!!!//
-  // $('#hearthpwn').css('height', $(window).height()+'px');
-
-  //MOVE DISBALED BUTTON FUNCTIONALITY TO A NEW "START OVER" A TAG//
-
-
-  //NEED TO CHANGE THIS SO THIS BUTTON APPEARS BEFORE PLATFORM IS CHOSEN//
-  $('.disabled').click(function(){
-    // console.log('start over fired');
-    // counter = 0;
-    // console.log(counter);
-    // $('#hearthpwn').attr('src', source[0]);
-  });
-
-  $('.primary').click(function(){
-    // console.log('next fired');
-    // counter++;
-    // console.log(counter);
-    // location.href = source[counter];
-  });
-
-  $('.secondary').click(function(){
-    // console.log('previous fired');
-    // counter--;
-    // console.log(counter);
-    // $('#hearthpwn').attr('src', source[counter]);
-  });
-
-
-
-});
+// REMOVE IF I DONT NEED JQUERY AT ALL
+// $(window).on('load', function(){
+//   //!!!!!!!! ENABLE FOR FINAL, DISABLED BC FIXED HEIGHT IS EASIER TO SEE !!!!!!!!//
+//   // $('#hearthpwn').css('height', $(window).height()+'px');
+//
+//   //MOVE DISBALED BUTTON FUNCTIONALITY TO A NEW "START OVER" A TAG//
+//
+//
+//   //NEED TO CHANGE THIS SO THIS BUTTON APPEARS BEFORE PLATFORM IS CHOSEN//
+//   $('.disabled').click(function(){
+//     // console.log('start over fired');
+//     // counter = 0;
+//     // console.log(counter);
+//     // $('#hearthpwn').attr('src', source[0]);
+//   });
+//
+//   $('.primary').click(function(){
+//     // console.log('next fired');
+//     // counter++;
+//     // console.log(counter);
+//     // location.href = source[counter];
+//   });
+//
+//   $('.secondary').click(function(){
+//     // console.log('previous fired');
+//     // counter--;
+//     // console.log(counter);
+//     // $('#hearthpwn').attr('src', source[counter]);
+//   });
+//
+//
+//
+// });
